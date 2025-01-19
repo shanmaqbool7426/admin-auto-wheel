2b945139b23b34194d70a3f0f165f76c7a9b56cb
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-19 20:18:20 +0500

    secnd commit

---
+++ src/app/(root)/blog/all-posts/layout.jsx
@@ -0,0 +1,11 @@
+
+
+export default function AllPostsLayout({ children }) {
+    return (
+        <html lang="en">
+            <body >
+                {children}
+            </body>
+        </html>
+    );
+}
