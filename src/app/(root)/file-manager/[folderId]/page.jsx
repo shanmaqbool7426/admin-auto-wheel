2c2e31dc174b58d038cf81cfeef6b72ec1396871
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 15:37:09 +0500

    live data

--- src/app/(root)/file-manager/[folderId]/page.jsx
+++ src/app/(root)/file-manager/[folderId]/page.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-import Loading from '@/components/Loading'; // Create this if you haven't
+import Files from '@/modules/Blog/FileManager';
 
-// Dynamically import the Files component with no SSR
-const Files = dynamic(() => import('@/modules/FileManager/Files'), {
-  ssr: false,
-  loading: () => <Loading />
-});
+
 
 // Mark the page component as a Client Component
 export default function FileManagerPage() {
