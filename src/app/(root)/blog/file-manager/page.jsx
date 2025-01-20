6ce47224ff8002188e7a95693719ce1e8d8ba668
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 14:17:02 +0500

    changes in apex chart

---
+++ src/app/(root)/blog/file-manager/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ClientWrapper from '@/components/ClientWrapper';
+
+export default function FileManagePage() {
+  return (
+    <ClientWrapper>
+      <div>FileManagePage</div>
+    </ClientWrapper>
+  )
+}
