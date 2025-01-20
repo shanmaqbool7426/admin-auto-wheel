75d1028c573d778197ed94ed5d93375a094ad131
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 13:26:53 +0500

    chnagesdebug

--- src/layouts/RootLayout/index.jsx
+++ src/layouts/RootLayout/index.jsx
@@ -11,7 +11,7 @@ import {
 import { useDisclosure } from '@mantine/hooks';
 import Navbar from './Navbar';
 import logo from '@/assets/images/brand/logo.svg';
-import Header from './Header';
+// import Header from './Header';
 import styles from './Rootlayout.module.css'
 
 export default function RootLayout({ children }) {
@@ -35,9 +35,9 @@ export default function RootLayout({ children }) {
         main: styles.main,
       }}
     >
-      <AppShell.Header>
+      {/* <AppShell.Header>
         <Header />
-      </AppShell.Header>
+      </AppShell.Header> */}
 
       <AppShell.Navbar p={0}>
         <AppShell.Section>
