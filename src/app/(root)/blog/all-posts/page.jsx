d152096b1e34f189c5b210767b1ac96540660498
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 14:11:38 +0500

    changes in apex chart

--- src/app/(root)/blog/all-posts/page.jsx
+++ src/app/(root)/blog/all-posts/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-// import AllPosts from '@/modules/Blog/AllPosts';
+import AllPosts from '@/modules/Blog/AllPosts';
 export default function AllPostsPage() {
-  return <h1>AllPostsPage</h1>;
+  return <AllPosts />
 }
+
