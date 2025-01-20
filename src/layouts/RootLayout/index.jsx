bd899509b155b896e576ee2fa4b868a241e62d6d
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 13:38:24 +0500

    chnagesdebug

--- src/layouts/RootLayout/index.jsx
+++ src/layouts/RootLayout/index.jsx
@@ -37,7 +37,8 @@ export default function RootLayout({ children }) {
     >
       <AppShell.Header>
         <Suspense fallback={<div>Loading...</div>}>
-          <Header />
+          {/* <Header /> */}
+          <></>
         </Suspense>
       </AppShell.Header>
 
