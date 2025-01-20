3ed7f3fbbb98c06c22d223c09321fbca8cf7e96d
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-20 13:24:19 +0500

    chnagesdebug

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import RootLayout from '@/layouts/RootLayout';
+import RootLayout from '@/layouts/RootLayout';
 
 export default function Rootlayout({ children }) {
   return (
-    // <RootLayout>
-    <>
+    <RootLayout>
       {children}
-    </>
-    // </RootLayout>
+    </RootLayout>
   )
 }
